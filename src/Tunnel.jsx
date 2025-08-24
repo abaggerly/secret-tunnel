a66b68a3c3995c4588bbc78e750323904013bec2
Amador Baggerly <amador.baggerly@gmail.com>
2025-08-23 21:55:46 -0500

    TroubleShooting

--- src/Tunnel.jsx
+++ src/Tunnel.jsx
@@ -1,7 +1,7 @@
 export default function Tunnel() {
   return (
     <section>
-      <p>The tablet vibrates and a jadeite chime rings through the air.</p>
+      <p>The tablet vibrates and a jadeite chime rings through the air. </p>
       <p>
         The earth beneath your feet trembles as the badgers slowly push the two
         doors of the gate open.
